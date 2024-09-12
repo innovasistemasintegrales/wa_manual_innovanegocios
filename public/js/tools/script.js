5f3eb1963fe53fdaedf2f1d0b669d68aef427ea7
Innova Sistemas Integrales <area.ti.innova@gmail.com>
2024-09-12 12:44:56 -0500

    Cambios modulo invitado

--- public/js/tools/script.js
+++ public/js/tools/script.js
@@ -72,4 +72,14 @@ function main(){
             cont = 1;
         }
     });
+
+    let boton = document.querySelector("#menuBtn");
+    let menu = document.querySelector("#sideMenu");
+
+    boton.addEventListener('click', e=>{
+        menu.classList.toggle("menu-expandido");
+        menu.classList.toggle("menu-colapsado");
+
+        document.querySelector('body').classList.toggle('body-expandido');
+    });
 }
