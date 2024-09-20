39ffb12c90c0ec6cd0790636f69151a05ef12d2f
J03l <innovatrainingti24.4@gmail.com>
2024-09-20 10:51:21 -0500

    cambioss viernes

--- public/js/cliente.js
+++ public/js/cliente.js
@@ -6,11 +6,12 @@ let cardReactivo = document.querySelector('#cardReactivo');
 
 const templateAsesoria = document.querySelector('#templateAsesoria').content;
 const templateConfiguracion = document.querySelector('#templateConfiguracion').content;
+const templateValoracion = document.querySelector('#templateValoracion').content;
 
 /* Etiqueta de botones de menu que vienen de HTMl */
 let btnMenuAsesoria = document.querySelector('#btnMenuAsesoria');
 let btnMenuConfiguracion = document.querySelector('#btnMenuConfiguracion');
-
+let btnMenuValoracion = document.querySelector('#btnMenuValoracion');
 
 
 
@@ -33,6 +34,19 @@ btnMenuConfiguracion.addEventListener('click', function () {
     templateConfiguracion.querySelector("#tituloConfiguracion").textContent = "Hola, soy el modulo configuración";
 
     const clone = templateConfiguracion.cloneNode(true);
+    fragmento.appendChild(clone);
+
+    cardReactivo.appendChild(fragmento);
+});
+
+/* Lanzamiento de la vista del menu Valoracion */
+btnMenuValoracion.addEventListener('click', function () {
+    cardReactivo.innerHTML = "";
+    
+
+    templateValoracion.querySelector("#tituloValoracion").textContent = "Hola, soy el modulo Valoracion";
+
+    const clone = templateValoracion.cloneNode(true);
     fragmento.appendChild(clone);
 
     cardReactivo.appendChild(fragmento);
