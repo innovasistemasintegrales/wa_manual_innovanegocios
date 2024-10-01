022603a10bd0a7ea5958567da5d6a4a723e8b975
Eduardo <eduardotanca@gmail.com>
2024-10-01 14:31:15 -0500

    Seccion Usuarios 90% - Asesoria 20% - Incidentes 1%

--- public/js/administrador.js
+++ public/js/administrador.js
@@ -9,62 +9,76 @@ const templateAsesoria = document.querySelector('#templateAsesoria').content;
 const templateValoracion = document.querySelector('#templateValoracion').content;
 const templateConfiguracion = document.querySelector('#templateConfiguracion').content;
 const templateUsuarios = document.querySelector('#templateUsuarios').content;
+const templateIncidentes = document.querySelector('#templateIncidentes').content;
 
 /* Etiqueta de botones de menu que vienen de HTMl */
 let btnMenuAsesoria = document.querySelector('#btnMenuAsesoria');
 let btnMenuConfiguracion = document.querySelector('#btnMenuConfiguracion');
 let btnMenuValoracion = document.querySelector('#btnMenuValoracion');
 let btnMenuUsuarios = document.querySelector('#btnMenuUsuarios');
+let btnMenuIncidentes = document.querySelector('#btnMenuIncidentes');
 
 /* Lanzamiento de la vista del menu Usuarios */
 btnMenuUsuarios.addEventListener('click', function () {
-  cardReactivo.innerHTML = "";
+    cardReactivo.innerHTML = "";
 
-  /* templateUsuarios.querySelector(".titulo-usuario").textContent = persona.nombre; */
+    /* templateUsuarios.querySelector(".titulo-usuario").textContent = persona.nombre; */
 
-  const clone = templateUsuarios.cloneNode(true);
-  fragmento.appendChild(clone);
+    const clone = templateUsuarios.cloneNode(true);
+    fragmento.appendChild(clone);
 
-  cardReactivo.appendChild(fragmento);
+    cardReactivo.appendChild(fragmento);
 });
 
 /* Lanzamiento de la vista del menu Asesoria */
 btnMenuAsesoria.addEventListener('click', function () {
-  cardReactivo.innerHTML = "";
+    cardReactivo.innerHTML = "";
 
-  /* templateAsesoria.querySelector(".titulo-asesoria").textContent = persona.nombre; */
+    /* templateAsesoria.querySelector(".titulo-asesoria").textContent = persona.nombre; */
 
-  const clone = templateAsesoria.cloneNode(true);
-  fragmento.appendChild(clone);
+    const clone = templateAsesoria.cloneNode(true);
+    fragmento.appendChild(clone);
 
-  cardReactivo.appendChild(fragmento);
+    cardReactivo.appendChild(fragmento);
+});
+
+/* Lanzamiento de la vista del menu Incidentes */
+btnMenuIncidentes.addEventListener('click', function () {
+    cardReactivo.innerHTML = "";
+
+    /* templateIncidentes.querySelector(".titulo-incidentes").textContent = persona.nombre; */
+
+    const clone = templateIncidentes.cloneNode(true);
+    fragmento.appendChild(clone);
+
+    cardReactivo.appendChild(fragmento);
 });
 
 /* Lanzamiento de la vista del menu valoración */
 btnMenuValoracion.addEventListener('click', function () {
-  cardReactivo.innerHTML = "";
+    cardReactivo.innerHTML = "";
 
-  /* templateValoracion.querySelector('#tituloValoracion').textContent = "Soy modulo valoración"; */
-  const clone = templateValoracion.cloneNode(true);
-  fragmento.appendChild(clone);
+    /* templateValoracion.querySelector('#tituloValoracion').textContent = "Soy modulo valoración"; */
+    const clone = templateValoracion.cloneNode(true);
+    fragmento.appendChild(clone);
 
-  cardReactivo.appendChild(fragmento);
+    cardReactivo.appendChild(fragmento);
 })
 
 /* Lanzamiento de la vista del menu configuración */
 btnMenuConfiguracion.addEventListener('click', function () {
-  cardReactivo.innerHTML = "";
+    cardReactivo.innerHTML = "";
 
-  // templateConfiguracion.querySelector("#tituloConfiguracion").textContent = "Hola, soy el modulo configuración";
+    // templateConfiguracion.querySelector("#tituloConfiguracion").textContent = "Hola, soy el modulo configuración";
 
-  const clone = templateConfiguracion.cloneNode(true);
-  fragmento.appendChild(clone);
+    const clone = templateConfiguracion.cloneNode(true);
+    fragmento.appendChild(clone);
 
-  cardReactivo.appendChild(fragmento);
+    cardReactivo.appendChild(fragmento);
 });
 
 
-// MARK: MODAL INCIDENTES
+// MARK: Modal Incidentes
 // Script para Manejar la Transición entre los Modales de la sección de Incidentes
 // Obtener referencias a los modales
 var modalIncidente = new bootstrap.Modal(document.getElementById('modalIncidente'));
@@ -74,13 +88,13 @@ var modalReasignar = new bootstrap.Modal(document.getElementById('modalReasignar
 var btnReasignar = document.querySelector('#modalIncidente .btn-reasignar');
 
 btnReasignar.addEventListener('click', function () {
-  // Cerrar el modal principal
-  modalIncidente.hide();
+    // Cerrar el modal principal
+    modalIncidente.hide();
 
-  // Esperar a que el modal principal se cierre antes de abrir el submodal
-  document.getElementById('modalIncidente').addEventListener('hidden.bs.modal', function () {
-    modalReasignar.show();
-  }, { once: true });
+    // Esperar a que el modal principal se cierre antes de abrir el submodal
+    document.getElementById('modalIncidente').addEventListener('hidden.bs.modal', function () {
+        modalReasignar.show();
+    }, { once: true });
 });
 
 // Botón para cancelar en el submodal y volver al modal principal
@@ -88,15 +102,15 @@ var botonesCancelarReasignar = document.querySelectorAll('.btnCancelarReasignar'
 
 botonesCancelarReasignar.forEach(boton => {
 
-  boton.addEventListener('click', function () {
-    // Cerrar el submodal
-    modalReasignar.hide();
+    boton.addEventListener('click', function () {
+        // Cerrar el submodal
+        modalReasignar.hide();
 
-    // Esperar a que el submodal se cierre antes de abrir el modal principal
-    document.getElementById('modalReasignar').addEventListener('hidden.bs.modal', function () {
-      modalIncidente.show();
-    }, { once: true });
-  });
+        // Esperar a que el submodal se cierre antes de abrir el modal principal
+        document.getElementById('modalReasignar').addEventListener('hidden.bs.modal', function () {
+            modalIncidente.show();
+        }, { once: true });
+    });
 });
 
 
@@ -137,67 +151,97 @@ function verificarContraseñas() {
 
 
 document.addEventListener('click', function (event) {
-  const btnEditar = document.querySelector('#btnEditarUsuario');
-  const btnGuardar = document.querySelector('#btnGuardarUsuario');
-  const btnCancelar = document.querySelector('#btnCancelarUsuario');
-  const inputs = document.querySelectorAll('.modal-usuario input');
-  const btnCerrar = document.querySelector('#btnCerrar');
+    const btnEditar = document.querySelector('#btnEditarUsuario');
+    const btnGuardar = document.querySelector('#btnGuardarUsuario');
+    const btnCancelar = document.querySelector('#btnCancelarUsuario');
+    const inputs = document.querySelectorAll('.modal-usuario input');
+    const btnCerrar = document.querySelector('#btnCerrar');
+    const customImageContainer = document.querySelector('#custom-image-container');
 
-  // Solo agrega los event listeners si los elementos existen
-  if (btnEditar && btnGuardar && btnCancelar) {
-    // Habilitar edición
-    btnEditar.addEventListener('click', () => {
-      inputs.forEach(input => input.disabled = false);
-      btnEditar.classList.add('d-none');
-      btnGuardar.classList.remove('d-none');
-      btnCancelar.classList.remove('d-none');
-      btnCerrar.classList.add('d-none');
-    });
+    // Solo agrega los event listeners si los elementos existen
+    if (btnEditar && btnGuardar && btnCancelar) {
+        // Habilitar edición
+        btnEditar.addEventListener('click', () => {
+            inputs.forEach(input => input.disabled = false);
+            btnEditar.classList.add('d-none');
+            btnGuardar.classList.remove('d-none');
+            btnCancelar.classList.remove('d-none');
+            btnCerrar.classList.add('d-none');
+            customImageContainer.classList.remove('d-none');
 
-    // Guardar cambios
-    btnGuardar.addEventListener('click', () => {
-      inputs.forEach(input => input.disabled = true);
-      btnEditar.classList.remove('d-none');
-      btnGuardar.classList.add('d-none');
-      btnCancelar.classList.add('d-none');
-      btnCerrar.classList.remove('d-none');
-    });
+            // Guardamos la imagen actual antes de hacer cambios
+            imagenOriginalSrc = document.getElementById('imgPerfilPreview').src;
+        });
 
-    // Cancelar edición
-    btnCancelar.addEventListener('click', () => {
-      inputs.forEach(input => input.disabled = true);
-      btnEditar.classList.remove('d-none');
-      btnGuardar.classList.add('d-none');
-      btnCancelar.classList.add('d-none');
-      btnCerrar.classList.remove('d-none');
-    });
-  } else {
-    console.error('Algunos de los botones no se encontraron en el DOM');
-  }
+        // Guardar cambios
+        btnGuardar.addEventListener('click', () => {
+            inputs.forEach(input => input.disabled = true);
+            btnEditar.classList.remove('d-none');
+            btnGuardar.classList.add('d-none');
+            btnCancelar.classList.add('d-none');
+            btnCerrar.classList.remove('d-none');
+            customImageContainer.classList.add('d-none');
+        });
+
+        // Cancelar edición
+        btnCancelar.addEventListener('click', () => {
+            inputs.forEach(input => input.disabled = true);
+            btnEditar.classList.remove('d-none');
+            btnGuardar.classList.add('d-none');
+            btnCancelar.classList.add('d-none');
+            btnCerrar.classList.remove('d-none');
+            customImageContainer.classList.add('d-none');
+
+            // Restaurar la imagen original
+            document.getElementById('imgPerfilPreview').src = imagenOriginalSrc;
+
+
+            // Restaurar la imagen original
+            document.getElementById('imgPerfilPreview').src = imagenOriginalSrc;
+        });
+    } else {
+        console.error('Algunos de los botones no se encontraron en el DOM');
+    }
 });
 
+// Scripts para mostrar la imagen de perfil subida en el modal de editar o agregar un nuevo usuario
+let imagenOriginalSrc = '';
 
-// MARK: MOSTRAR Y OCULATR SECCIONES DE ASESORIA
+// Función para previsualizar la imagen subida
+function previewImagenPerfil(event) {
+    const file = event.target.files[0];
+    if (file) {
+        const reader = new FileReader();
+        reader.onload = function (e) {
+            document.getElementById('imgPerfilPreview').src = e.target.result;
+        }
+        reader.readAsDataURL(file);
+    }
+}
+
+
+
+// MARK: Mostrar y Ocultar Secciones de Asesoria
 document.addEventListener('click', () => {
 
-  const radioFAQ = document.querySelector('#menu-radio-faq');
-  const radioManual = document.querySelector('#menu-radio-manual');
-  const seccionFAQ = document.querySelector('#seccion-faq');
-  const seccionManual = document.querySelector('#seccion-manual');
+    const radioFAQ = document.querySelector('#menu-radio-faq');
+    const radioManual = document.querySelector('#menu-radio-manual');
+    const seccionFAQ = document.querySelector('#seccion-faq');
+    const seccionManual = document.querySelector('#seccion-manual');
 
-  radioFAQ.addEventListener('click', () => {
-    if (radioFAQ.checked) {
-      seccionFAQ.classList.remove('d-none');
-      seccionManual.classList.add('d-none');
-    }
-  });
+    radioFAQ.addEventListener('click', () => {
+        if (radioFAQ.checked) {
+            seccionFAQ.classList.remove('d-none');
+            seccionManual.classList.add('d-none');
+        }
+    });
 
-  radioManual.addEventListener('click', () => {
-    if (radioManual.checked) {
-      seccionFAQ.classList.add('d-none');
-      seccionManual.classList.remove('d-none');
-    }
-  });
+    radioManual.addEventListener('click', () => {
+        if (radioManual.checked) {
+            seccionFAQ.classList.add('d-none');
+            seccionManual.classList.remove('d-none');
+        }
+    });
 
 })
 
