d8f21360299f60df22953cecf21294efddf880ed
J03l <innovatrainingti24.4@gmail.com>
2024-10-07 14:30:46 -0500

    cambios casi listo

--- public/js/soporte.js
+++ public/js/soporte.js
@@ -37,6 +37,41 @@ btnMenuIncidentes.addEventListener('click', function () {
 
   cardReactivo.appendChild(fragmento);
 
+  // MARK: MODAL INCIDENTES
+  // Script para Manejar la Transición entre los Modales de la sección de Incidentes
+  // Obtener referencias a los modales
+  var modalIncidente = new bootstrap.Modal(document.getElementById('modalIncidente'));
+  var modalReasignar = new bootstrap.Modal(document.getElementById('modalReasignar'));
+
+  // Botón para abrir el submodal desde el modal principal
+  var btnReasignar = document.querySelector('#modalIncidente .btn-reasignar');
+
+  btnReasignar.addEventListener('click', function () {
+    // Cerrar el modal principal
+    modalIncidente.hide();
+
+    // Esperar a que el modal principal se cierre antes de abrir el submodal
+    document.getElementById('modalIncidente').addEventListener('hidden.bs.modal', function () {
+      modalReasignar.show();
+    }, { once: true });
+  });
+
+  // Botón para cancelar en el submodal y volver al modal principal
+  var botonesCancelarReasignar = document.querySelectorAll('.btnCancelarReasignar');
+
+  botonesCancelarReasignar.forEach(boton => {
+
+    boton.addEventListener('click', function () {
+      // Cerrar el submodal
+      modalReasignar.hide();
+
+      // Esperar a que el submodal se cierre antes de abrir el modal principal
+      document.getElementById('modalReasignar').addEventListener('hidden.bs.modal', function () {
+        modalIncidente.show();
+      }, { once: true });
+    });
+  });
+
 });
 
 /* Lanzamiento de la vista del menu configuración */
@@ -86,27 +121,3 @@ botonesCancelarReasignar.forEach(boton => {
     }, { once: true });
   });
 });
-
-// MARK: MOSTRAR Y OCULATR SECCIONES DE ASESORIA
-document.addEventListener('click', () => {
-
-  const menureportesclientes = document.querySelector('#menu-reportes-clientes');
-  const menureportestecnicos = document.querySelector('#menu-reportes-tecnicos');
-  const seccionreportesclientes = document.querySelector('#seccion-reportes-clientes');
-  const seccionreportestecnicos = document.querySelector('#seccion-reportes-tecnicos');
-
-  menureportesclientes.addEventListener('click', () => {
-    if (menureportesclientes.checked) {
-      seccionreportesclientes.classList.remove('d-none');
-      seccionreportestecnicos.classList.add('d-none');
-    }
-  });
-
-  menureportestecnicos.addEventListener('click', () => {
-    if (menureportestecnicos.checked) {
-      seccionreportesclientes.classList.add('d-none');
-      seccionreportestecnicos.classList.remove('d-none');
-    }
-  });
-
-})
