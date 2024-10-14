1f78723746bd6a88b8628bff11374657044c11ef
Eduardo Tanca <eduardotanca@gmail.com>
2024-10-13 22:03:29 -0500

    .

--- public/js/administrador.js
+++ public/js/administrador.js
@@ -86,30 +86,19 @@ var modalReasignar = new bootstrap.Modal(document.getElementById('modalReasignar
 
 // Botón para abrir el submodal desde el modal principal
 var btnReasignar = document.querySelector('#modalIncidente .btn-reasignar');
-
 btnReasignar.addEventListener('click', function () {
     // Cerrar el modal principal
     modalIncidente.hide();
-
-    // Esperar a que el modal principal se cierre antes de abrir el submodal
-    document.getElementById('modalIncidente').addEventListener('hidden.bs.modal', function () {
-        modalReasignar.show();
-    }, { once: true });
+    modalReasignar.show();
 });
 
 // Botón para cancelar en el submodal y volver al modal principal
 var botonesCancelarReasignar = document.querySelectorAll('.btnCancelarReasignar');
-
 botonesCancelarReasignar.forEach(boton => {
-
     boton.addEventListener('click', function () {
         // Cerrar el submodal
+        modalIncidente.show();
         modalReasignar.hide();
-
-        // Esperar a que el submodal se cierre antes de abrir el modal principal
-        document.getElementById('modalReasignar').addEventListener('hidden.bs.modal', function () {
-            modalIncidente.show();
-        }, { once: true });
     });
 });
 
