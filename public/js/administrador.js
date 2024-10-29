1714dd4e22e61689de14d7ec71867e2341f86929
Eduardo <eduardotanca@gmail.com>
2024-10-29 13:53:47 -0500

    93%

--- public/js/administrador.js
+++ public/js/administrador.js
@@ -116,11 +116,11 @@ botonesCancelarReasignar.forEach(boton => {
 
 
 //TODO  MARK: Sección Usuarios 
-// Registrar usuario
+// Registrar usuario   
 const formRegistroUsuario = document.getElementById('modalRegistrarUsuario');
 const btnRegistrarUsuario = formRegistroUsuario.querySelector('#btnRegistrarUsuario');
 const btnCancelarRegistro = formRegistroUsuario.querySelector('#btnCancelarRegistro');
-btnRegistrarUsuario.addEventListener('click', registrarUsuario);
+btnRegistrarUsuario.addEventListener('click', registrarUsuario(formRegistroUsuario));
 btnCancelarRegistro.addEventListener('click', () => limpiarFormulario(formRegistroUsuario));
 
 document.addEventListener("DOMContentLoaded", function () {
@@ -201,8 +201,9 @@ function validarCampo(input) {
     return isValid;
 }
 
-function registrarUsuario() {
+function registrarUsuario(formRegistroUsuario) {
     let rolSeleccionado = formRegistroUsuario.querySelector('input[name="seleccionRol"]:checked');
+
     if (rolSeleccionado) {
         formRegistroUsuario.querySelector('.divSeleccionRol').classList.remove('is-invalid');
         formRegistroUsuario.querySelector('.divSeleccionRol p').classList.remove('is-invalid');
@@ -242,10 +243,13 @@ function registrarUsuario() {
                         nacimiento,
                         estado,
                     }
+                    //? Implementación de la imagen perfil (pendiente)
                     // if (imagenPerfil) {
                     //     nuevoUsuario.append(imagenPerfil);
                     // }
+
                     // socket.emit('/administrador/registrarUsuario', nuevoUsuario);
+
                     alert("Formulario enviado");
                     limpiarFormulario();
                     mostrarAlerta('¡El usuario ha sido registrado exitosamente!', 'success', 3000);
@@ -270,17 +274,9 @@ function registrarUsuario() {
     }
 }
 
-// Función para mostrar el error
-function mostrarError(input, mensaje) {
-    input.classList.add('is-invalid');
-    const feedbackElement = input.nextElementSibling;
-    if (feedbackElement && feedbackElement.classList.contains('invalid-feedback')) {
-        feedbackElement.textContent = mensaje;
-    }
-}
 
 // Función para limpiar el formulario
-function limpiarFormulario() {
+function limpiarFormulario(formRegistroUsuario) {
     formRegistroUsuario.querySelectorAll('.form-control').forEach(input => {
         input.value = "";
         input.classList.remove('is-valid', 'is-invalid');
@@ -439,7 +435,7 @@ btnMenuAsesoria.addEventListener('click', () => {
                 // Verificar si los botones "Editar" y "Eliminar" ya existen
                 if (!document.querySelector(`#accordionItem${newId} .edit-btn`)) {
                     const editBtn = document.createElement('button');
-                    editBtn.classList.add('btn', 'btn-primary', 'edit-btn');
+                    editBtn.classList.add('btn', 'btn-color-1', 'edit-btn');
                     editBtn.textContent = 'Editar';
 
                     const deleteBtn = document.createElement('button');
@@ -577,6 +573,204 @@ btnMenuValoracion.addEventListener('click', () => {
     }
 })
 
+
+// TODO MARK: Sección Configuración
+
+btnMenuConfiguracion.addEventListener("click", () => {
+    // Selecciona el contenedor de configuración o el template que se muestra al hacer click
+    const formConfiguracionUsuario = document.getElementById('editarUsuarioConfiguracion');
+
+    // Botones dentro de la sección de configuración
+    const btnEditarUsuario = formConfiguracionUsuario.querySelector('#editButton');
+    const btnCancelarEdicion = formConfiguracionUsuario.querySelector('#cancelButton');
+    const btnGuardarCambios = formConfiguracionUsuario.querySelector('#saveButton');
+
+    // Estado original para restaurar al cancelar
+    let estadoOriginal = {};
+
+    // Listeners para los botones de editar, cancelar y guardar
+    btnEditarUsuario.addEventListener('click', () => habilitarEdicion(formConfiguracionUsuario));
+    btnCancelarEdicion.addEventListener('click', () => cancelarEdicion(formConfiguracionUsuario, estadoOriginal));
+    btnGuardarCambios.addEventListener('click', () => guardarCambios(btnGuardarCambios, formConfiguracionUsuario));
+
+    // Guardar estado original al iniciar la configuración
+    guardarEstadoOriginal(formConfiguracionUsuario, estadoOriginal);
+
+    // Validación en tiempo real para los inputs
+    formConfiguracionUsuario.querySelectorAll('input:not([type="file"]):not(#nacimientoUsuario):not([type="radio"])').forEach(input => {
+        input.addEventListener('input', () => {
+            const esValido = validarCampoConfiguracion(input);
+            verificarCambios(formConfiguracionUsuario, estadoOriginal, btnGuardarCambios);
+            btnGuardarCambios.disabled = !esValido;
+        });
+    });
+});
+
+// Función para habilitar la edición de los campos
+function habilitarEdicion(formConfiguracionUsuario) {
+    formConfiguracionUsuario.querySelectorAll('input').forEach(input => {
+        if (input.id !== 'fechaIngresoUsuario' && input.id !== 'estadoUsuario') {
+            input.disabled = false
+        }
+    }
+    );
+    document.getElementById('cancelButton').classList.remove('d-none');
+    document.getElementById('saveButton').classList.remove('d-none');
+    document.getElementById('saveButton').disabled = true; // Solo activado si hay cambios
+    document.getElementById('editButton').classList.add('d-none');
+}
+
+// Función para guardar el estado actual de los campos
+function guardarEstadoOriginal(form, estadoOriginal) {
+    form.querySelectorAll('input').forEach(input => {
+        estadoOriginal[input.id] = input.value;
+    });
+}
+
+// Función para revertir los cambios y desactivar edición
+function cancelarEdicion(formConfiguracionUsuario, estadoOriginal) {
+    formConfiguracionUsuario.querySelectorAll('input').forEach(input => {
+        input.value = estadoOriginal[input.id]; // Revertir al valor inicial
+        input.classList.remove('is-valid', 'is-invalid');
+        input.disabled = true;
+    });
+    document.getElementById('cancelButton').classList.add('d-none');
+    document.getElementById('saveButton').classList.add('d-none');
+    document.getElementById('editButton').classList.remove('d-none');
+}
+
+// Verificar si hay cambios en los campos
+function verificarCambios(form, estadoOriginal, btnGuardarCambios) {
+    const hayCambios = Array.from(form.querySelectorAll('input')).some(input => {
+        return input.value !== estadoOriginal[input.id];
+    });
+    btnGuardarCambios.disabled = !hayCambios;
+}
+
+// Función para validar cada campo individual
+function validarCampoConfiguracion(input) {
+    let esValido = true;
+    const feedbackElement = input.nextElementSibling;
+    input.classList.remove('is-valid', 'is-invalid');
+
+    // Validaciones específicas por campo
+    switch (input.id) {
+        case 'correoUsuario':
+            esValido = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(input.value);
+            if (!esValido) mostrarError(input, 'Ingrese un correo electrónico válido');
+            break;
+        case 'dniUsuario':
+            esValido = input.value.length === 8 && /^\d+$/.test(input.value);
+            if (!esValido) mostrarError(input, 'El DNI debe tener 8 dígitos numéricos');
+            break;
+        case 'telefonoUsuario':
+            esValido = input.value.length === 9 && /^\d+$/.test(input.value);
+            if (!esValido) mostrarError(input, 'El teléfono debe tener 9 dígitos numéricos');
+            break;
+        default:
+            esValido = input.value.trim() !== '';
+            if (!esValido) mostrarError(input, 'Este campo es obligatorio');
+    }
+
+    if (esValido) input.classList.add('is-valid');
+    return esValido;
+}
+
+// Mostrar mensaje de error
+function mostrarError(input, mensaje) {
+    input.classList.add('is-invalid');
+    const feedbackElement = input.nextElementSibling;
+    if (feedbackElement && feedbackElement.classList.contains('invalid-feedback')) {
+        feedbackElement.textContent = mensaje;
+    }
+}
+
+function guardarCambios(btnGuardarCambios, formConfiguracionUsuario) {
+    if (!btnGuardarCambios.disabled) {
+        actualizarDatosUsuario(formConfiguracionUsuario);
+
+    }
+}
+
+// Deshabilitar la edición y ocultar botones Guardar y Cancelar
+function deshabilitarEdicion(formConfiguracionUsuario) {
+    formConfiguracionUsuario.querySelectorAll('input').forEach(input => {
+        input.classList.remove('is-valid', 'is-invalid');
+        input.disabled = true;
+    });
+    document.getElementById('cancelButton').classList.add('d-none');
+    document.getElementById('saveButton').classList.add('d-none');
+    document.getElementById('editButton').classList.remove('d-none');
+}
+
+function actualizarDatosUsuario(form) {
+
+    let nombre = form.querySelector("#nombreUsuario").value;
+    let correo = form.querySelector("#correoUsuario").value;
+    let usuario = form.querySelector("#userUsuario").value;
+    let password = form.querySelector("#passwordUsuario").value;
+    let dni = form.querySelector("#dniUsuario").value;
+    let telefono = form.querySelector("#telefonoUsuario").value;
+    let direccion = form.querySelector("#direccionUsuario").value;
+    let fechaIngreso = form.querySelector("#fechaIngresoUsuario").value;
+    let nacimiento = form.querySelector("#nacimientoUsuario").value;
+    let estado = form.querySelector("#estadoUsuario").value;
+    //let imagenPerfil = formRegistroUsuario.querySelector('#addImg').files[0]; // Capturamos el archivo de imagen
+    let expresiones = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    let valido = expresiones.test(correo);
+
+    if (nombre !== "" && correo !== "" && usuario !== "" && password !== "" && dni !== "" && telefono !== "" && direccion !== "" && estado !== "") {
+        if (valido === true) {
+            if (telefono.length == 9) {
+                if (dni.length == 8) {
+                    let usuarioActualizado = {
+                        rolSeleccionado,
+                        nombre,
+                        correo,
+                        usuario,
+                        password,
+                        dni,
+                        telefono,
+                        direccion,
+                        fechaIngreso,
+                        nacimiento,
+                        estado,
+                    }
+                    //? Implementación de la imagen perfil (pendiente)
+                    // if (imagenPerfil) {
+                    //     nuevoUsuario.append(imagenPerfil);
+                    // }
+
+                    // socket.emit('/administrador/registrarUsuario', nuevoUsuario);
+                    showConfirmModal('Confirmar cambios', '¿Estás seguro de que deseas guardar los cambios?', 'Guardar cambios', function () {
+                        console.log('Cambios guardados con éxito');
+                        deshabilitarEdicion(formConfiguracionUsuario);
+                        mostrarAlerta('¡Los cambios se han guardado con éxito!', 'success', 3000);
+                    });
+
+                }
+                else {
+                    mostrarError(form.querySelector('#dniUsuario'), "El DNI debe tener 8 dígitos");
+                }
+            }
+            else {
+                mostrarError(form.querySelector('#telefonoUsuario'), "El teléfono debe tener 9 dígitos");
+            }
+        }
+        else {
+            mostrarError(form.querySelector('#correoUsuario'), 'Ingrese un correo electrónico válido');
+        }
+    }
+    else {
+        form.querySelectorAll('input:not(#fecha-ingreso):not([type="file"]):not(#nacimiento):not([type="radio"])').forEach(input => {
+            validarCampoConfiguracion(input)
+        });
+    }
+
+}
+
+
+
 //TODO MARK: Sección Reportes
 // Manejo del menu de reportes
 btnMenuReportes.addEventListener('click', () => {
@@ -711,7 +905,7 @@ function mostrarAlerta(mensaje, tipo = 'success', duracion = 3000) {
     alerta.textContent = mensaje;
 
     // Añadir la alerta al DOM (por ejemplo, al principio del body o dentro de un contenedor específico)
-    const containerElement = document.querySelector('#');
+    const containerElement = document.body;
     document.body.appendChild(alerta);
 
     // Ocultar la alerta después del tiempo especificado (duracion en milisegundos)
