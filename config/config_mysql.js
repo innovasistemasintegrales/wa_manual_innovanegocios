1f8bca6b895cf304c8e0faff37186397ffda4981
Innova Sistemas Integrales <area.ti.innova@gmail.com>
2024-11-12 10:19:32 -0500

    Base de datos agregada

--- config/config_mysql.js
+++ config/config_mysql.js
@@ -1,10 +1,21 @@
-import mysql from 'mysql'
-try {
-    const conecction = mysql.createConnection({
-        host: '',
-        user: '',
-        password: '',
-    })
-} catch (error) {
+/* import mysql from 'mysql'; */
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+    host: 'localhost',
+    database: 'db_manual_innova',
+    user: 'root',
+    password: '',
+})
+
+connection.connect((err) =>{
+    if (err) {
+        console.error('Error al conectar la base de datos', err);
+        throw err;        
+    }
+    console.log('Conexión a base de datos exitosa');
     
-}
+})
+
+
+module.exports = connection;
